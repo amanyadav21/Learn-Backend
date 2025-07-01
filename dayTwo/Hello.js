e9be24fba572ac558f2b9bfc3fb3cf8830421e07
amanyadav21 <amankumaryadav1029@gmail.com>
2025-07-01 22:29:19 +0530

    now learn express js some basic

--- dayTwo/Hello.js
+++ dayTwo/Hello.js
@@ -1,1 +1,18 @@
-console.log('hello owrl')
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send('This is Main page Section');
+});
+
+app.get('/home', (req, res) => {
+    res.send('This is home page');
+});
+
+app.get('/about', (req, res) => {
+    res.send('This is your about page');
+});
+
+app.listen(3000, () => {
+    console.log('Your server is start now');
+});
