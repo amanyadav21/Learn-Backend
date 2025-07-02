a9c30b72f6b1ecee2e9f7ae1426b3fcc86341a11
amanyadav21 <amankumaryadav1029@gmail.com>
2025-07-02 12:58:52 +0530

    learn basic concept of express js creating a server

--- dayTwo/Hello.js
+++ dayTwo/Hello.js
@@ -1,18 +1,26 @@
-const express = require('express');
-const app = express();
+const express = require('express')
+
+const app = express()
 
 app.get('/', (req, res) => {
-    res.send('This is Main page Section');
-});
+    res.end('This is Home section')
+})
 
-app.get('/home', (req, res) => {
-    res.send('This is home page');
-});
+app.get('/home',(req, res) => {
+    res.end('This is New Home page section')
+})
 
-app.get('/about', (req, res) => {
-    res.send('This is your about page');
-});
+app.get('/about',(req, res) => {
+    res.end('This is about section')
+})
 
+app.get('/blog',(req, res) => {
+    res.end('This is Blog section at section')
+})
+
+app.get('/carrer', (req, res) => {
+    res.end('This is carrer section')
+})
 app.listen(3000, () => {
-    console.log('Your server is start now');
-});
+    console.log('Port are Start Now')
+})
