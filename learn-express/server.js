afe639a8adcd7ce39f7cf449575a55dda1b6c562
amanyadav21 <amankumaryadav1029@gmail.com>
2025-08-08 13:19:11 +0530

    learn how to work dynamic rounting in express

--- learn-express/server.js
+++ learn-express/server.js
@@ -2,15 +2,25 @@ const express = require('express')
 const app =  express()
 
 
-app.use(function(req, res, next) {
-    // next()
-})
+// app.use(function(req, res, next) {
+//     console.log('Middlewere working now')
+//     next()
+// })
 
 
 
 
 app.get('/',(req, res) => {
     res.send('Ha Bhai teri request ayi hai sever tak')
+    console.log(req)
+})
+
+app.get('/profile',function(res, req) {
+    req.send('Hello form Profile')
+})
+
+app.get('/profile/:username', function(req, res) {
+    res.send(`Hello from ${req.params.username}`)
 })
 
 
