13f9d20af139fe16e11a91c7d04db0c04cd83ae8
amanyadav21 <amankumaryadav1029@gmail.com>
2025-12-28 14:50:57 +0530

    facial - detection - working and give result based on face emotion

--- moody/frontend/src/components/facialexpression.jsx
+++ moody/frontend/src/components/facialexpression.jsx
@@ -1,29 +1,93 @@
-import React, { use, useEffect, useRef, useState } from 'react'
+import React, { useEffect, useRef, useState } from 'react'
+import * as faceapi from "face-api.js";
+import './common.css'
 
-const facialexpression = () => {
 
-    const videoRef = useRef(null)
-    const canvaRef = useRef(null)
-    const [modelsLoaded, setModelsLoaded] = useState(false)
-    const [currentEmotion, setCurrentEmotion] = useState('')
-    const [isDetecting, setIsDetecting] = useState(false)
+const FacialExpression = () => {
+    // Step 1: Refs & States
 
-    useEffect(() => {
-        const loadModels = async () => {
-            const MODEL_URL = '/models'
+    const videoRef = useRef(null);
+    const [emotion, setEmotion] = useState("")
+    const [modelsLoaded, setModelsLoaded] = useState(false);
 
-            try {
-                await Promise.all({
-                    facialexpression.tinyFaceDetector.loadFromUri(MODEL_URL)
-                    facialexpression
-                })
+    // Step 2: Camera Starting Function
+    const startCamera = async () => {
+        try {
+            const stream = await navigator.mediaDevices.getUserMedia({video: true});
+            videoRef.current.srcObject = stream;
+        } catch (error) {
+            console.log("Camera Error:", error);
+        }
+    };
+
+    // Step 3 Load Face api Models
+    const loadFaceModels = async () => {
+        const MODEL_URL = "/models";
+
+        await faceapi.nets.tinyFaceDetector.loadFromUri(MODEL_URL);
+        await faceapi.nets.faceExpressionNet.loadFromUri(MODEL_URL);
+
+        setModelsLoaded(true);
+    };
+
+    // STEP 4 Detect Mood Fuction
+
+    const detectMood = async () => {
+        if(!modelsLoaded) {
+            alert("Models not loaded yet!");
+            return;
+        }
+
+        const result = await faceapi.detectSingleFace(
+            videoRef.current,
+            new faceapi.TinyFaceDetectorOptions()
+        ).withFaceExpressions();
+
+        if(!result) {
+            setEmotion("No Face Detected")
+            return;
+        }
+
+        const expressions = result.expressions;
+
+        let maxValue = 0;
+        let detectedEmotion = "";
+
+        for(let mood in expressions) {
+            if(expressions[mood] > maxValue) {
+                maxValue = expressions[mood];
+                detectedEmotion = mood;
             }
         }
-    })
 
-  return (
-    <div>facialexpression</div>
+        setEmotion(detectedEmotion.toUpperCase());
+    };
+
+useEffect(() => {
+    loadFaceModels();
+    startCamera();
+}, [])
+
+return (
+    <div id='main'>
+        <h1>Facial Mood Detector!</h1>
+        <video
+        ref={videoRef}
+        autoPlay
+        muted
+        width="518"
+        height="390"
+        />
+
+        <br />
+
+        <button onClick={detectMood}>
+            Detect Mood
+        </button>
+
+        <h3>Mood: {emotion}</h3>
+    </div>
   )
 }
 
-export default facialexpression
+export default FacialExpression
