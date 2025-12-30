fbd46e027d4943588d3e47586aee41ed95fadd23
amanyadav21 <amankumaryadav1029@gmail.com>
2025-12-30 16:28:55 +0530

    add logout feature in previous code

--- day134_auth/src/routes/auth.routes.js
+++ day134_auth/src/routes/auth.routes.js
@@ -36,39 +36,11 @@ router.post('/register', async (req, res) => {
 })
 
 
-router.post('/login', async (req, res) => {
-    const {username, password} = req.body
-    /*isme aye hai ki pata karna hoga ki user phle se hai ki nahi */
-
-
-    const isUserExist = await userModel.findOne({
-        username:username
-    })
-
-    if(!isUserExist) {
-        return res.status(401).json({
-            message:"user account not found"
-        })
-    }
-
-    const isPasswordValid = password == isUserExist.password
-
-    if(!isPasswordValid) {
-        return res.status(401).json({
-            message: "Invalid Password"
-        })
-    }
-
-    res.status(200).json({
-        message:"User loggedIn successfully"
-    })
-
-})
-
-
-router.get('/user', async (req, res) => {
+router.get('/user', async (req, res) => { // [Protected]
     // const {token} = req.body
     const {token} = req.cookies
+    // aya check karna hoga ki token kis name se save kiye ho or fir usi name se token ready karna hoga
+    // const {token} = req.cookies.chacha
 
     if(!token) {
         return res.status(401).json({
@@ -79,6 +51,8 @@ router.get('/user', async (req, res) => {
     // jwt verify aye check karti hai ki tumara tokon sahi hai ki nahi
     try{
         const decoded = jwt.verify(token,process.env.JWT_SECRET)
+        // jwt.verify - aye check karti hai ki joh token apne bje hai
+        // usko hamare ki server ne create kiya hai ki nahi
 
         const user = await userModel.findOne({
             _id:decoded.id
@@ -96,8 +70,54 @@ router.get('/user', async (req, res) => {
             message: "Unauthorized - Invalid token"
         })
     }
-
      
 })
+
+
+router.post('/login', async (req, res) => {
+    const {username, password} = req.body
+    /*isme aye hai ki pata karna hoga ki user phle se hai ki nahi */
+
+
+    const isUserExist = await userModel.findOne({
+        username:username
+    })
+
+    if(!isUserExist) {
+        return res.status(404).json({
+            message:"user account not found"
+        })
+    }
+
+    const isPasswordValid = password == isUserExist.password
+
+    if(!isPasswordValid) {
+        return res.status(401).json({
+            message: "Invalid Password"
+        })
+    }
+
+    const token = jwt.sign({id:user._id}.process.env.JWT_SECRET)
+
+    res.cookie("chacha", token , {
+        expires: new Date(Date.now() + 1000 * 60 * 60 * 7),
+    })
+
+    res.status(200).json({
+        message:"User loggedIn successfully",
+        user
+    })
+
+})
+
+router.get('/logout', async (req, res) => {
+    res.clearCookie("chacha")
+
+    res.status(200).json({
+        message: "user logged out successfully"
+    })
+})
+
+
 
 export default router
