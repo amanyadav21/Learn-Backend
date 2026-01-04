02cf5bbe9b10a1334be3f9f2a5904066da2e1437
amanyadav21 <amankumaryadav1029@gmail.com>
2026-01-04 13:34:50 +0530

    day138 - complete and learn how to db password secure via the help of bcrypt - and create register and login controller and import in routes or use it

--- everytest/src/app.js
+++ everytest/src/app.js
@@ -2,6 +2,7 @@ import express from 'express'
 import connectTodb from './db/db.js'
 
 import authRoutes from './routes/auth.routes.js'
+import cookieParser from 'cookie-parser'
 
 import dotenv from 'dotenv'
 dotenv.config()
@@ -9,6 +10,8 @@ dotenv.config()
 
 
 const app = express()
+app.use(cookieParser())
+app.use(express.json())
 app.use('/api/auth', authRoutes);
 
 
--- everytest/src/controllers/auth.controller.js
+++ everytest/src/controllers/auth.controller.js
@@ -1,8 +1,70 @@
+import userModel from '../models/user.model.js'
+import JWT from 'jsonwebtoken'
+import bcrypt from 'bcryptjs'
+
 
 export const registerController = async (req, res) => {
     const {username, password} = req.body
+    
+    const isUserAlreadyExist = await userModel.findOne({
+        username: username
+    })
+
+    if(isUserAlreadyExist){
+        return res.status(200).send({
+            success: false,
+            message: 'User already exists'
+        })
+    }
+
+    const newUser = await userModel.create({
+        username,
+        password: await bcrypt.hash(password, 10)
+    })
+
+    const token = JWT.sign({id:newUser._id},process.env.JWT_SECRET)
+
+    res.cookie("token", token)
+
+    return res.status(201).json({
+        success: true,
+        message: "User registered successfully",
+        newUser
+    })
 }
 
-export const userController = async (req, res) => {
-    const {username, password} = req.body
+
+export const loginController = async (req, res) => {
+    const {username, password} = req.body;
+
+    const user = await userModel.findOne({
+        username
+    })
+
+    if(!user) {
+        return res.status(400).json({
+            message: "User not found"
+        })
+    }
+
+    const isPasswordValid = bcrypt.compare(password, user.password);
+
+    if(!isPasswordValid) {
+        return res.status(400).json({
+            message: "Invalid password"
+        })
+    }
+
+    const token = JWT.sign({id:user._id}, process.env.JWT_SECRET)
+
+    res.cookie("token", token)
+
+    res.status(200).json({
+        success: true,
+        message: "User logged in successfully",
+        user:{
+            username: user.username,
+            id: user._id
+        }
+    })
 }
