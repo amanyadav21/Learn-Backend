e471a20ee24e39a841ade1f503efead8f40a6277
amanyadav21 <amankumaryadav1029@gmail.com>
2026-01-12 09:28:59 +0530

    learn the basic of - socket.io custom event

--- day142_aichatbot/server.js
+++ day142_aichatbot/server.js
@@ -8,7 +8,19 @@ const httpServer = createServer();
 const io = new Server(httpServer, { /* options */ });
 
 io.on("connection", (socket) => {
-  // ...
+    console.log("A user connected!")
+
+    socket.on("disconnect", () => {
+      console.log("A user disconnect")
+    })
+
+    socket.on("message", () => {
+      console.log("message reveived")
+    })
+
+    socket.on("mama", () => {
+      console.log("Mama koh message mil gya")
+    })
 });
 
 httpServer.listen(3000, () => {
